57e0c5f4a76f60793df97e2e36f310b136799912
Sreesha-Amjuri <sreeshaamjuri@gmail.com>
2019-09-09 15:44:04 +0530

    terraform file

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+ access_key= "AKIAJTYU4CMZXBFLD7OA"
+ secret_key= "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" 
+ region = "us-east-1"
+}
+
+
+resource "aws_vpc" "My_VPC" {
+  cidr_block           = "10.0.0.0/16"
+  instance_tenancy     = "default" 
+  enable_dns_support   = "true" 
+  enable_dns_hostnames = "true" 
+ 
+}

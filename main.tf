9d9b5c38884a11b84117f3d2aaab34209cab9157
Sreesha-Amjuri <sreeshaamjuri@gmail.com>
2019-09-09 16:58:23 +0530

    aws version change

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
+ version = "~> 2.0"
  access_key= "AKIAJTYU4CMZXBFLD7OA"
  secret_key= "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" 
  region = "us-east-1"
 }
-
 
 resource "aws_vpc" "My_VPC" {
   cidr_block           = "10.0.0.0/16"
